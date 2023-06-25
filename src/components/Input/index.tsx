15490a36dfe81b49a5c6ad97b89b8323eaeb57af
felipebuch <felipe.m.buch@gmail.com>
2023-06-25 11:15:31 -0300

    Finalizando Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,10 @@
 import { TextInputProps } from "react-native";
 import { Container } from "./styles";
+import { useTheme } from "styled-components/native";
 
 export function Input({ ...rest }: TextInputProps) {
-  return <Container {...rest}></Container>;
+  const { COLORS } = useTheme();
+  return (
+    <Container {...rest} placeholderTextColor={COLORS.GRAY_300}></Container>
+  );
 }
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -14,7 +14,7 @@ export function NewGroup() {
           title="Nova Turma"
           subtitle=" Crie uma turma para adicionar as pessoas"
         />
-        <Input />
+        <Input placeholder="Nome da turma" />
         <Button title="Criar" />
       </Content>
     </Container>
