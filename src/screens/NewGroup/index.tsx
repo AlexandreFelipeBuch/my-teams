0c899da6e99cbaa361cd4dcf25fd33a730c84ac6
felipebuch <felipe.m.buch@gmail.com>
2023-06-30 19:48:41 -0300

    tratando erros

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,4 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
+import { Alert } from "react-native";
 import { Header } from "@components/Header";
 import { Container, Content, Icon } from "./styles";
 import { HighLight } from "@components/HighLight";
@@ -6,14 +7,26 @@ import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { useState } from "react";
 import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
 
 export function NewGroup() {
   const [group, setGroup] = useState("");
   const { navigate } = useNavigation();
 
   async function handleNew() {
-    await groupCreate(group);
-    navigate("players", { group });
+    try {
+      if (group.trim().length === 0) {
+        return Alert.alert("Novo Grupo", "Informe o nome da turma");
+      }
+      await groupCreate(group);
+      navigate("players", { group });
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo Grupo", "Não foi possivel criar um novo grupo");
+      }
+    }
   }
   return (
     <Container>
